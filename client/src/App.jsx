0fd964c98adc7e1f2829ccfa77b46d65b6e176ce
Shawn Weed <shawn.weed@icloud.com>
2024-07-09 11:55:20 -0400

    Templates for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='SignIn' element={<SignIn />} />
+        <Route path='SignUp' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
