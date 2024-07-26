a536667217edcfb943a363a34d03ba7aff8a89ae
Shawn Weed <shawn.weed@icloud.com>
2024-07-26 10:39:13 -0400

    Added ability to show comments on post page

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js'
 import { createComment } from '../controllers/comment.controllers.js';
+import { getPostComments } from '../controllers/comment.controllers.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createComment)
+router.post('/create', verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,12 +1,15 @@
 import { Alert, Button, Textarea } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Comment from './Comment';
 
 export default function CommentSection( {postId} ) {
     const { currentUser } = useSelector(state => state.user);
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
+    const [comments, setComments] = useState([])
+    console.log(comments)
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -23,12 +26,29 @@ export default function CommentSection( {postId} ) {
         const data = await res.json();
         if (res.ok) {
             setComment('');
-            setCommentError(null)
+            setCommentError(null);
+            setComments([data, ...comments]);
         }
       } catch (error) {
         setCommentError(error.messssage);
       }
     }
+
+    useEffect(() => {
+        const getComments = async () => {
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if (res.ok){
+                    const data = await res.json();
+                    setComments(data);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getComments();
+    }, [postId])
+
   return (
     <div className='max-w-2xl mx-auto w-full'>
         {currentUser ?
@@ -76,6 +96,25 @@ export default function CommentSection( {postId} ) {
             </Alert> 
         )}
         </form>
+    )}
+    {comments.length === 0 ? (
+        <p className='text-sm my-5'>No comments yet! Be the first</p>
+    ):(
+        <>
+        <div className='text-sm my-5 flex items-center gap-1'>
+          <p>Comments</p>
+            <div>
+              <p className='border border-gray-400 py-1 px-2 rounded-sm'>{comments.length}</p>
+            </div>
+        </div>
+        {
+          comments.map(comment => (
+            <Comment 
+            key={comment._id} comment=
+            {comment} />
+          ))
+        }
+        </>
     )}
     </div>
   )
