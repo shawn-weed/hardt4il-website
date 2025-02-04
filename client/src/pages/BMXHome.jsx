e549d7b999db4162d7b6469382e5f781e5e2b90e
Shawn Weed <127861884+shawn-weed@users.noreply.github.com>
2025-02-03 19:44:23 -0500

    Added message for empty content BMX

--- client/src/pages/BMXHome.jsx
+++ client/src/pages/BMXHome.jsx
@@ -17,7 +17,7 @@ export default function BMXHome() {
   return (
     <div>
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
-        {posts && posts.length > 0 && (
+        {posts && posts.length > 0 ? (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>Recent BMX Posts</h2>
             <div className='flex flex-wrap gap-4 justify-center'>
@@ -32,7 +32,7 @@ export default function BMXHome() {
               View All Posts
             </Link>
           </div>
-        )}
+        ) : <h1 className='text-4xl'>Content coming soon!</h1>}
       </div>
     </div>
   );
